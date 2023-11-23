704d47788b9999fda4afcc2a73e3fe69d1ff0958
Marta Frances <marta.frapin@educa.jcyl.es>
2023-11-23 22:43:37 +0100

    cambios 23/11 22:43

--- wiki.js
+++ wiki.js
@@ -1,11 +1,13 @@
-window.onload= function () {
+window.onload = function () {
 
     demoOperaciones();
     demoDate();
     mostrarInformacionCoche();
     cargarDatosGatos();
+    saludar("Marta");
 
 }
+
 
 function demoOperaciones() {
     let num1 = 10;
@@ -29,14 +31,14 @@ function demoDate() {
 class Vehiculo {
     constructor(marca) {
         this.marca = marca;
-        }
- }
-                            
+    }
+}
+
 class Coche extends Vehiculo {
-     constructor(marca, modelo) {
+    constructor(marca, modelo) {
         super(marca);
         this.modelo = modelo;
-        }
+    }
 }
 
 function mostrarInformacionCoche() {
@@ -97,5 +99,111 @@ function mostrarGatosDeRaza(razaSeleccionada) {
         .catch(error => console.error('Error al cargar los datos de gatos:', error));
 }
 
+let saludar = function (nombre) {
+    let mensaje = "Hola " + nombre;
+    document.getElementById("mensajeSaludo").textContent = mensaje;
+};
 
+document.addEventListener('DOMContentLoaded', function () {
+    var formulario = document.getElementById('formulario');
+    formulario.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let input = document.getElementById("nombre").value;
+        let mensaje = "Texto recibido: " + input;
+        document.getElementById("formularioresponse").textContent = mensaje;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    localStorage.setItem("clave", "aaaa");
+    const valorAlmacenado = localStorage.getItem("clave");
+    document.getElementById("kuki").textContent = valorAlmacenado;
+});
+
+
+fetch('https://api.covidtracking.com/v2/us/daily/2021-01-02/simple.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Respuesta de red no fue ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const divDatosCovid = document.getElementById('datosCovid');
+        // Aquí puedes formatear los datos como desees antes de mostrarlos
+        divDatosCovid.textContent = JSON.stringify(data, null, 2); // Muestra los datos en formato JSON
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos:', error);
+        document.getElementById('datosCovid').textContent = 'Error al cargar los datos';
+    });
+
+
+function validarEmail(valor) {
+    let emailInput = document.querySelector("#inputEmail");
+    let pass = document.querySelector("#inputpassword");
+
+    // Expresión regular para validar el correo electrónico
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (regexEmail.test(valor)) {
+        console.log("La dirección de correo electrónico es válida");
+        emailInput.classList.add("inputValido");
+        emailInput.classList.remove("inputInvalido");
+        pass.removeAttribute("disabled");
+
+        // Asegurarse de que no se añadan múltiples event listeners
+        pass.removeEventListener("input", validarPassHandler);
+        pass.addEventListener("input", validarPassHandler);
+    } else {
+        console.log("La dirección de correo electrónico no es válida");
+        emailInput.classList.add("inputInvalido");
+        emailInput.classList.remove("inputValido");
+        pass.setAttribute("disabled", true);
+    }
+}
+
+function validarPassHandler(event) {
+    let valorPass = event.target.value;
+    validarPass(valorPass); // Asegúrate de que esta función esté definida
+}
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('botonCopiar').addEventListener('click', function () {
+        // Obtener el texto del elemento div
+        var texto = document.getElementById('textoParaCopiar').textContent;
+
+        // Crear un elemento textarea temporal
+        var textarea = document.createElement('textarea');
+        textarea.textContent = texto;
+        document.body.appendChild(textarea);
+
+        // Seleccionar el texto del textarea
+        textarea.select();
+
+        try {
+            // Copiar el texto seleccionado
+            var exitoso = document.execCommand('copy');
+            var msg = exitoso ? 'exitoso' : 'no exitoso';
+            console.log('Copiado ' + msg);
+        } catch (err) {
+            console.log('Oops, no se pudo copiar');
+        }
+
+        // Eliminar el textarea temporal
+        document.body.removeChild(textarea);
+    });
+});
+
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById("password");
+    var passwordToggle = document.getElementById("password-toggle");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        passwordToggle.textContent = 'visibility';
+    } else {
+        passwordField.type = "password";
+        passwordToggle.textContent = 'visibility_off';
+    }
+}
 
