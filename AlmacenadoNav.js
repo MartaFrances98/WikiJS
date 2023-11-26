0e4f67e8edba8cbad0ac394f3e5701dad0a031ed
Marta Frances <marta.frapin@educa.jcyl.es>
2023-11-26 15:00:10 +0100

    cambios 26/11 15:00

---
+++ AlmacenadoNav.js
@@ -0,0 +1,80 @@
+window.onload = function () {
+
+    crearCookie();
+    mostrarValorCookie();
+
+}
+
+//1. Coockie
+function crearCookie() {
+    var fechaExpiracion = new Date();
+    fechaExpiracion.setTime(fechaExpiracion.getTime() + (7 * 24 * 60 * 60 * 1000));
+    document.cookie = "nombreUsuario=Marta; expires=" + fechaExpiracion.toUTCString() + "; path=/";
+    console.log(document.cookie); // Agregado para depuración
+}
+
+function obtenerCookie(nombre) {
+    var nombreIgual = nombre + "=";
+    var arrayCookies = document.cookie.split(';');
+    for(var i = 0; i < arrayCookies.length; i++) {
+        var c = arrayCookies[i];
+        while (c.charAt(0) === ' ') c = c.substring(1);
+        if (c.indexOf(nombreIgual) === 0) {
+            return c.substring(nombreIgual.length, c.length);
+        }
+    }
+    return "";
+}
+
+function mostrarValorCookie() {
+    var valorCookie = obtenerCookie("nombreUsuario"); // Obtiene el valor de la cookie
+    var elementoP = document.getElementById("contenidoCookie");
+    elementoP.textContent = "El valor de la cookie es: " + valorCookie;
+}
+
+function eliminarCookie(nombre) {
+    document.cookie = nombre + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/';
+}
+
+//2. LocalStorage
+document.addEventListener('DOMContentLoaded', function () {
+    localStorage.setItem("clave", "aaaa");
+    const valorAlmacenado = localStorage.getItem("clave");
+    document.getElementById("kuki").textContent = valorAlmacenado;
+});
+
+//3. IndexBD
+    var solicitudDB = window.indexedDB.open("ejemplobasedatoswikijs", 1);
+
+    solicitudDB.onerror = function(event) {
+        document.getElementById('resultadoDB').textContent += "Error al abrir la base de datos: " + event.target.errorCode + "\n";
+    };
+    
+    solicitudDB.onupgradeneeded = function(event) {
+        var db = event.target.result;
+        var objectStore = db.createObjectStore("login", { keyPath: "id" });
+        objectStore.createIndex("nombre", "nombre", { unique: false });
+        objectStore.createIndex("email", "email", { unique: true });
+    };
+    
+    solicitudDB.onsuccess = function(event) {
+        var db = event.target.result;
+        var transaccion = db.transaction("login", "readwrite");
+        var objectStore = transaccion.objectStore("login");
+    
+        var solicitudAgregar = objectStore.add({ id: 1, nombre: "Marta", email: "marta@example.com" });
+        solicitudAgregar.onsuccess = function(event) {
+            document.getElementById('resultadoDB').textContent += "Datos agregados a la base de datos\n";
+        };
+    
+        var solicitudLeer = objectStore.get(1);
+        solicitudLeer.onsuccess = function(event) {
+            if (solicitudLeer.result) {
+                document.getElementById('resultadoDB').textContent += "Nombre: " + solicitudLeer.result.nombre + ", Email: " + solicitudLeer.result.email + "\n";
+            } else {
+                document.getElementById('resultadoDB').textContent += "No se encontró el registro\n";
+            }
+        };
+    };
+
+
---
+++ SolicitudRed.js
@@ -0,0 +1,98 @@
+window.onload = function () {
+    cargarDatosGatos();
+
+}
+function cargarDatosGatos() {
+    fetch('gatos.json')
+        .then(response => response.json())
+        .then(datosGatos => {
+            llenarSelectorConRazas(datosGatos);
+        })
+        .catch(error => console.error('Error al cargar los datos de gatos:', error));
+}
+
+function llenarSelectorConRazas(datosGatos) {
+    const selectorRaza = document.getElementById('selectorRaza');
+    const razas = [...new Set(datosGatos.map(gato => gato.raza))];
+
+    razas.forEach(raza => {
+        const opcion = document.createElement('option');
+        opcion.value = raza;
+        opcion.textContent = raza;
+        selectorRaza.appendChild(opcion);
+    });
+}
+
+function mostrarGatosDeRaza(razaSeleccionada) {
+    fetch('gatos.json')
+        .then(response => response.json())
+        .then(datosGatos => {
+            const gatosFiltrados = datosGatos.filter(gato => gato.raza === razaSeleccionada);
+            const tabla = document.getElementById('tablaGatos');
+            tabla.innerHTML = ''; // Limpiar tabla anterior
+
+            if (gatosFiltrados.length) {
+                const encabezado = tabla.insertRow();
+                ['Nombre', 'Raza', 'Edad', 'Color'].forEach(texto => {
+                    const celda = encabezado.insertCell();
+                    celda.textContent = texto;
+                });
+
+                gatosFiltrados.forEach(gato => {
+                    const fila = tabla.insertRow();
+                    Object.values(gato).forEach(texto => {
+                        const celda = fila.insertCell();
+                        celda.textContent = texto;
+                    });
+                });
+            }
+        })
+        .catch(error => console.error('Error al cargar los datos de gatos:', error));
+}
+
+fetch('https://api.covidtracking.com/v2/us/daily/2021-01-02/simple.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Respuesta de red no fue ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const divDatosCovid = document.getElementById('datosCovid');
+        // Aquí puedes formatear los datos como desees antes de mostrarlos
+        divDatosCovid.textContent = JSON.stringify(data, null, 2); // Muestra los datos en formato JSON
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos:', error);
+        document.getElementById('datosCovid').textContent = 'Error al cargar los datos';
+    });
+
+    fetch('https://api.example.com/data', {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('resultadoFetch').textContent = 'Datos recibidos: ' + JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        document.getElementById('resultadoFetch').textContent = 'Error al obtener datos: ' + error;
+    });
+    
+    fetch('https://api.example.com/submit-data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            key1: 'value1',
+            key2: 'value2'
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('resultadoFetch').textContent += '\nDatos enviados con éxito: ' + JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+        document.getElementById('resultadoFetch').textContent += '\nError al enviar datos: ' + error;
+    });
+    
